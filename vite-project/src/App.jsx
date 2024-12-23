4cbe38d8661ec0d158b4d2b8bb301135e032ed82
Student ApnaCollege <rs5768827@gmail.com>
2024-12-24 03:26:57 +0530

    second

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-import {BrowserRouter, Route, RouterProvider} from 'react-router-dom'
-import Header from './components/header';
-import HomePage from './pages/HomePage';
-import {CoinsPage} from './pages/CoinsPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import {CoinsPage }from "./pages/CoinsPage";
+import { colors, makeStyles } from '@material-ui/core';
+
 function App() {
-  
+
 
   return (
-     <BrowserRouter>
-     <div>
-      <Header />
-      <Route path='/' Component={HomePage}></Route>
-      <Route path='/Coins/:id' Component={CoinsPage}></Route>
-     </div>
-     </BrowserRouter>
-        
-  )
+    <Router>
+      <div className='text-white min-h-screen'>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Coins/:id" element={<CoinsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+
+
 
 export default App
--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,10 +1,29 @@
+import { AppBar, Container, createTheme, Menu, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
 
 const Header = () => {
+  const darkTheme = createTheme({
+    palette: {
+      type :'dark',
+    },
+});
   return (
-    <div>
-      HEADER
-    </div>
+   < ThemeProvider theme={darkTheme}>
+    <AppBar color='transparent' position='static'>
+      <Container>
+        <Toolbar>
+          <Typography className='text-lime-300 font-bold font-mono flex cursor-pointer'>
+          CryptoScope
+          </Typography>
+          <Select variant='outlined' style={{width:100,height:40,marginLeft:15,}}>
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"INR"}>INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    </ThemeProvider>
+    
   )
 }
 
