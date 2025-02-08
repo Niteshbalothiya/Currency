4b749c2f352c9b0310fc447e22b56143c84e8e8b
Niteshbalothiya <rs5768827@gmail.com>
2025-02-09 00:16:46 +0530

    error fix

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/header'; // Import Header component
-import HomePage from './pages/HomePage'; // Import HomePage
-import CoinsPages from './pages/CoinsPages'; // Import CoinsPages
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import CoinsPages from './pages/CoinsPages';
 
 const App = () => {
   return (
     <div className="bg-gray-900 min-h-screen text-white">
       <Router>
-        {/* Render the Header at the top */}
         <Header />
         {/* Define Routes */}
         <Routes>
